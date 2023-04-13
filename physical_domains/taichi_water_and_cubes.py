5ebc96119c628691fa21f9f043633bcfcb24b1c1
David Schneider-Joseph <david@davidsj.com>
2023-04-13 22:19:01 +0000

    simple water and cubes demo using the MPM solver in Taichi Elements

---
+++ physical_domains/taichi_water_and_cubes.py
@@ -0,0 +1,48 @@
+import os
+import taichi as ti
+import numpy as np
+import math
+from taichi_elements.engine.mpm_solver import MPMSolver
+import argparse
+
+# This script is adapted from taichi_elements/demo/demo_2d.py.
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--out-dir', type=str, help='Output folder')
+    args = parser.parse_args()
+    print(args)
+    return args
+
+
+args = parse_args()
+
+write_to_disk = args.out_dir is not None
+if write_to_disk:
+    os.mkdir(f'{args.out_dir}')
+
+ti.init(arch=ti.cuda)  # Try to run on GPU
+
+gui = ti.GUI("Taichi Elements", res=512, background_color=0x112F41)
+
+mpm = MPMSolver(res=(128, 128))
+
+for i in range(3):
+    mpm.add_cube(lower_corner=[0.2 + i * 0.1, 0.3 + i * 0.1],
+                 cube_size=[0.1, 0.1],
+                 material=MPMSolver.material_elastic)
+
+for frame in range(500):
+    mpm.step(8e-3)
+    if 10 < frame < 200:
+        mpm.add_cube(lower_corner=[0.6, 0.7],
+                     cube_size=[0.2, 0.01],
+                     material=MPMSolver.material_water,
+                     velocity=[math.sin(frame * 0.1), 0])
+    colors = np.array([0x068587, 0xED553B, 0xEEEEF0, 0xFFFF00],
+                      dtype=np.uint32)
+    particles = mpm.particle_info()
+    gui.circles(particles['position'],
+                radius=1.5,
+                color=colors[particles['material']])
+    gui.show(f'{args.out_dir}/{frame:06d}.png' if write_to_disk else None)
