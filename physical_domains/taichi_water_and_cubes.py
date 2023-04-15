745f72b06cdcf17ef3906159642a547e9fcc8300
David Schneider-Joseph <david@davidsj.com>
2023-04-15 15:08:04 +0000

    WaterAndCubes dataset generation script

--- physical_domains/taichi_water_and_cubes.py
+++ physical_domains/taichi_water_and_cubes.py
@@ -1,48 +1,100 @@
 import os
 import taichi as ti
 import numpy as np
-import math
 from taichi_elements.engine.mpm_solver import MPMSolver
 import argparse
+import json
+import io
+import contextlib
 
-# This script is adapted from taichi_elements/demo/demo_2d.py.
+# Parse arguments.
+parser = argparse.ArgumentParser()
+parser.add_argument('--out_dir', type=str, default='datasets/taichi_water_and_cubes')
+parser.add_argument('--traj_len', type=int, default=1000)
+parser.add_argument('--n_training', type=int, default=10)
+parser.add_argument('--n_validation', type=int, default=2)
+parser.add_argument('--n_test', type=int, default=2)
+parser.add_argument('--dt_ms', type=float, default=2.5)
+parser.add_argument('--n_cubes', type=int, default=2)
+parser.add_argument('--cube_size', type=float, default=0.1)
+parser.add_argument('--water_size', type=float, default=0.4)
+args = parser.parse_args()
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-o', '--out-dir', type=str, help='Output folder')
-    args = parser.parse_args()
-    print(args)
-    return args
+# Validate bounds on arguments.
+assert args.traj_len >= 0
+assert args.n_training >= 0
+assert args.n_validation >= 0
+assert args.n_test >= 0
+assert args.dt_ms > 0
+assert args.n_cubes >= 0
+assert args.cube_size > 0
+assert args.water_size > 0
 
+# Make sure cubes and water fit horizontally across.
+assert args.cube_size * args.n_cubes <= 1.0
+assert args.water_size <= 1.0
+# Make sure cubes plus water fit vertically.
+assert args.water_size + args.cube_size <= 1.0
 
-args = parse_args()
+# Make sure the output dir doesn't already exist and then create it.
+assert not os.path.exists(args.out_dir)
+os.makedirs(args.out_dir)
 
-write_to_disk = args.out_dir is not None
-if write_to_disk:
-    os.mkdir(f'{args.out_dir}')
+# Write metadata as JSON.
+with open(os.path.join(args.out_dir, 'metadata.json'), 'w') as f:
+    md = vars(args)
+    md['name'] = 'taichi_water_and_cubes'
+    md['dimensions'] = 2
+    md['n_materials'] = len(MPMSolver.materials)
+    json.dump(md, f)
 
-ti.init(arch=ti.cuda)  # Try to run on GPU
+# Create the trajectories for each dataset.
+for dataset in ['training', 'validation', 'test']:
+    n_trajs = args.__dict__[f'n_{dataset}']
+    os.makedirs(os.path.join(args.out_dir, dataset))
 
-gui = ti.GUI("Taichi Elements", res=512, background_color=0x112F41)
+    for traj_idx in range(n_trajs):
+        positions = []
+        materials = []
 
-mpm = MPMSolver(res=(128, 128))
+        ti.init(arch=ti.cuda)
+        mpm = MPMSolver(res=(64, 64))
 
-for i in range(3):
-    mpm.add_cube(lower_corner=[0.2 + i * 0.1, 0.3 + i * 0.1],
-                 cube_size=[0.1, 0.1],
-                 material=MPMSolver.material_elastic)
+        # Create the water as high up as possible.
+        min_left = 0.0
+        max_left = 1.0 - args.water_size
+        left = np.random.uniform(min_left, max_left)
+        bottom = 1.0 - args.water_size
+        mpm.add_cube(lower_corner=[left, bottom],
+                    cube_size=[args.water_size, args.water_size],
+                    material=MPMSolver.material_water)
 
-for frame in range(500):
-    mpm.step(8e-3)
-    if 10 < frame < 200:
-        mpm.add_cube(lower_corner=[0.6, 0.7],
-                     cube_size=[0.2, 0.01],
-                     material=MPMSolver.material_water,
-                     velocity=[math.sin(frame * 0.1), 0])
-    colors = np.array([0x068587, 0xED553B, 0xEEEEF0, 0xFFFF00],
-                      dtype=np.uint32)
-    particles = mpm.particle_info()
-    gui.circles(particles['position'],
-                radius=1.5,
-                color=colors[particles['material']])
-    gui.show(f'{args.out_dir}/{frame:06d}.png' if write_to_disk else None)
+        # Create the cubes below the water.
+        for i in range(args.n_cubes):
+            min_left = i/args.n_cubes
+            max_left = (i+1)/args.n_cubes - args.cube_size
+            left = np.random.uniform(min_left, max_left)
+
+            min_bottom = 0.0
+            max_bottom = 1.0 - args.water_size - args.cube_size
+            bottom = np.random.uniform(min_bottom, max_bottom)
+
+            mpm.add_cube(lower_corner=[left, bottom],
+                        cube_size=[args.cube_size, args.cube_size],
+                        material=MPMSolver.material_elastic)
+        print(f'{dataset} trajectory {traj_idx:5d}: created {len(mpm.particle_info()["position"])} particles')
+
+        # Simulate.
+        for frame in range(args.traj_len):
+            with io.StringIO() as buffer, contextlib.redirect_stdout(buffer):
+                # We redirect mpm's stdout because it prints a lot of dots and
+                # there's no way to turn that off.
+                mpm.step(args.dt_ms / 1000.0)
+            particles = mpm.particle_info()
+            positions.append(particles['position'])
+            materials.append(particles['material'])
+        
+        # Save the trajectory.
+        np.savez_compressed(os.path.join(args.out_dir, dataset, f'{traj_idx}.npz'),
+                            positions=np.array(positions),
+                            materials=np.array(materials))
