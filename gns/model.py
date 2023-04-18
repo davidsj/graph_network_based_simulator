803f06884bf3d5a4b2888ea90217b8f5a28ec3ab
David Schneider-Joseph <david@davidsj.com>
2023-04-18 18:33:42 +0000

    support for particle boundary wall distance features in Encoder

--- gns/model.py
+++ gns/model.py
@@ -23,34 +23,48 @@ class Encoder(torch.nn.Module):
     """Encodes a datapoint into the appropriate graph representation, excluding acceleration
     (which is the prediction target)."""
     def __init__(self, n_materials, physical_dim, n_previous_velocities,
+                 connectivity_radius, box_boundaries=None,
                  material_embedding_dim=16, node_embedding_dim=128, edge_embedding_dim=128):
         super().__init__()
         self.n_materials = n_materials
         self.physical_dim = physical_dim
         self.n_previous_velocities = n_previous_velocities
+        self.connectivity_radius = connectivity_radius
+        self.box_boundaries = box_boundaries
 
         # Materials are embedded from a one-hot representation.
         self.material_encoder = torch.nn.Linear(n_materials, material_embedding_dim)
 
-        # Nodes are encoded from a concatenation of the previous velocities and
-        # material of the corresponding particle.
-        self.node_encoder = MLP(n_previous_velocities*physical_dim + material_embedding_dim,
-                                 node_embedding_dim)
+        # Nodes are encoded from a concatenation of the previous velocities,
+        # material of the corresponding particle, and if orthogonal walls are
+        # present, the distance to them, clipped to `connectivity_radius`.
+        node_feature_dim = n_previous_velocities*physical_dim + material_embedding_dim
+        if box_boundaries is not None:
+            node_feature_dim += 2*physical_dim
+        self.node_encoder = MLP(node_feature_dim, node_embedding_dim)
         self.node_layer_norm = torch.nn.LayerNorm(node_embedding_dim)
-        
+
         # Edges are encoded from a concatenation of the relative position and distance
         # between the two neighboring particles.
         self.edge_encoder = MLP(physical_dim + 1, edge_embedding_dim)
         self.edge_layer_norm = torch.nn.LayerNorm(edge_embedding_dim)
-    
+
     def forward(self, dp):
         # Make a one-hot representation of the materials, then embed them.
         materials = torch.nn.functional.one_hot(dp.materials, self.n_materials).to(torch.float32)
         materials = self.material_encoder(materials)
 
-        # Concatenate the positions, velocities, and materials.
+        # Concatenate the velocities, materials, and if orthogonal walls are present,
+        # the distance to them, clipped to `connectivity_radius`.
         velocities = dp.velocities.reshape(-1, self.n_previous_velocities*self.physical_dim)
         node_features = torch.cat([velocities, materials], dim=1)
+        if self.box_boundaries is not None:
+            wall_dist = torch.zeros(node_features.shape[0], 2*self.physical_dim)
+            for i in range(self.physical_dim):
+                wall_dist[:, 2*i] = dp.positions[:, i] - self.box_boundaries[i][0]
+                wall_dist[:, 2*i + 1] = self.box_boundaries[i][1] - dp.positions[:, i]
+            wall_dist = torch.clamp(wall_dist, min=0.0, max=self.connectivity_radius)
+            node_features = torch.cat([node_features, wall_dist], dim=1)
         nodes = self.node_encoder(node_features)
         nodes = self.node_layer_norm(nodes)
 
