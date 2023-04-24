8f0c493ab4498950ed37693c87086ab28103a5d9
David Schneider-Joseph <david@davidsj.com>
2023-04-24 06:51:08 +0000

    model support for batched data

--- gns/model.py
+++ gns/model.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 from scipy.spatial import KDTree
+from .trajectory import TorchDatapoint
 
 class MLP(torch.nn.Module):
     """Shared MLP architecture for encoder, processor, and decoder."""
@@ -65,39 +66,64 @@ class Encoder(torch.nn.Module):
         self.edge_encoder = FeatureEncoder(physical_dim + 1, edge_embedding_dim)
 
     def forward(self, dp):
-        n_particles = dp.materials.shape[0]
+        if isinstance(dp, TorchDatapoint):
+            dps = [dp]
+        else:
+            dps = dp
+        # This currently assumes all datapoints in the batch have the
+        # same number of particles. TODO: Pad with dummy particles.
+        #
+        # N = number of datapoints
+        # P = number of particles
+        # D = physical dimension
+        # V = number of previous velocities
+        mat = torch.stack([dp.materials for dp in dps])  # N x P
+        pos = torch.stack([dp.positions for dp in dps])  # N x P x D
+        vel = torch.stack([dp.velocities for dp in dps]) # N x P x V x D
+        n_datapoints, n_particles = mat.shape
+        device = mat.device
 
         # Make a one-hot representation of the materials, then embed them.
-        materials = torch.nn.functional.one_hot(dp.materials, self.n_materials).to(torch.float32)
+        materials = torch.nn.functional.one_hot(mat, self.n_materials).to(torch.float32)
         materials = self.material_encoder(materials)
 
         # Concatenate the velocities, materials, and (if present) distance
         # to orthogonal walls, clipped to `connectivity_radius`.
-        velocities = dp.velocities.reshape(-1, self.n_previous_velocities*self.physical_dim)
-        node_features = torch.cat([velocities, materials], dim=1)
+        velocities = vel.reshape(n_datapoints, n_particles, -1)
+        node_features = torch.cat([velocities, materials], dim=-1)
         if self.box_boundaries is not None:
-            wall_dist = torch.zeros(n_particles, 2*self.physical_dim, device=node_features.device)
+            wall_dist = torch.zeros(n_datapoints, n_particles, self.physical_dim, 2,
+                                    device=device)
             for i in range(self.physical_dim):
-                wall_dist[:, 2*i] = dp.positions[:, i] - self.box_boundaries[i][0]
-                wall_dist[:, 2*i + 1] = self.box_boundaries[i][1] - dp.positions[:, i]
+                wall_dist[:, :, i, 0] = pos[:, :, i] - self.box_boundaries[i][0]
+                wall_dist[:, :, i, 1] = self.box_boundaries[i][1] - pos[:, :, i]
             wall_dist = torch.clamp(wall_dist, min=0.0, max=self.connectivity_radius)
-            node_features = torch.cat([node_features, wall_dist], dim=1)
+            wall_dist = wall_dist.reshape(n_datapoints, n_particles, -1)
+            node_features = torch.cat([node_features, wall_dist], dim=-1)
         nodes = self.node_encoder(node_features)
 
-        # Identify edges. TODO: Do on GPU?
-        if self.connectivity_radius in dp.neighbor_idx_cache:
-            neighbor_idxs = dp.neighbor_idx_cache[self.connectivity_radius].to(dp.positions.device)
-        else:
-            pos_np = dp.positions.detach().cpu().numpy()
-            kdtree = KDTree(pos_np)
-            neighbors = kdtree.query_ball_point(pos_np, self.connectivity_radius)
-            neighbor_idxs = torch.tensor([(i, j) for i in range(n_particles) for j in neighbors[i] if i != j],
-                                        device=dp.positions.device, dtype=torch.int64)
-            neighbor_idxs = neighbor_idxs.reshape((-1, 2)) # ensure the right shape in case there are no neighbors
-            dp.neighbor_idx_cache[self.connectivity_radius] = neighbor_idxs.to('cpu')
+        # Identify edges. TODO: Do on GPU or at least parallelize across datapoints?
+        neighbor_idxs = []
+        for i, dp in enumerate(dps):
+            if self.connectivity_radius in dp.neighbor_idx_cache:
+                dp_neighbor_idxs = dp.neighbor_idx_cache[self.connectivity_radius].to(device)
+            else:
+                pos_np = pos[i].detach().cpu().numpy()
+                kdtree = KDTree(pos_np)
+                neighbors = kdtree.query_ball_point(pos_np, self.connectivity_radius)
+                dp_neighbor_idxs = torch.tensor([(j, k) for j in range(n_particles) for k in neighbors[j] if j != k],
+                                            device=device, dtype=torch.int64)
+                dp_neighbor_idxs = dp_neighbor_idxs.reshape((-1, 2)) # ensure the right shape in case there are no neighbors
+                dp.neighbor_idx_cache[self.connectivity_radius] = dp_neighbor_idxs.to('cpu')
+            datapoint_idx = torch.tensor([[i]], device=device).broadcast_to(dp_neighbor_idxs.shape[0], 1)
+            dp_neighbor_idxs = torch.cat([datapoint_idx, dp_neighbor_idxs], dim=1)
+            neighbor_idxs.append(dp_neighbor_idxs)
+        neighbor_idxs = torch.cat(neighbor_idxs, dim=0) # (datapoint_idx, receiver_idx, sender_idx)
 
         # Compute the relative positions and distances.
-        relative_positions = dp.positions[neighbor_idxs[:, 0]] - dp.positions[neighbor_idxs[:, 1]]
+        receiver_pos = pos[neighbor_idxs[:, 0], neighbor_idxs[:, 1]]
+        sender_pos = pos[neighbor_idxs[:, 0], neighbor_idxs[:, 2]]
+        relative_positions = receiver_pos - sender_pos
         distances = torch.norm(relative_positions, dim=1, keepdim=True)
         edge_features = torch.cat([relative_positions, distances], dim=1)
         edges = self.edge_encoder(edge_features)
@@ -128,19 +154,23 @@ class Processor(torch.nn.Module):
         for edge_updater, edge_layer_norm, node_updater, node_layer_norm in zip(
             self.edge_updaters, self.edge_layer_norms, self.node_updaters, self.node_layer_norms):
             # Update the edges.
-            receivers = nodes[neighbor_idxs[:, 0]]
-            senders = nodes[neighbor_idxs[:, 1]]
-            edges_out = edge_updater(torch.cat([edges, receivers, senders], dim=1))
+            receivers = nodes[neighbor_idxs[:, 0], neighbor_idxs[:, 1]]
+            senders = nodes[neighbor_idxs[:, 0], neighbor_idxs[:, 2]]
+            edges_out = edge_updater(torch.cat([edges, receivers, senders], dim=-1))
             edges_out = edge_layer_norm(edges_out)
             edges = edges + edges_out
 
-            # Aggregate the edges for each node.
-            aggregated_edges = torch.zeros(nodes.shape[0], edges.shape[1], device=edges.device)
-            index = neighbor_idxs[:, [0]].broadcast_to(edges.shape)
+            # Aggregate the edges for each node. We do this with nodes flattened
+            # across datapoints so we can use scatter_add.
+            aggregated_edges = torch.zeros(nodes.shape[0]*nodes.shape[1], edges.shape[-1],
+                                           device=edges.device)
+            index = neighbor_idxs[:, [0]]*nodes.shape[1] + neighbor_idxs[:, [1]]
+            index = index.broadcast_to(edges.shape)
             aggregated_edges = aggregated_edges.scatter_add(0, index, edges)
+            aggregated_edges = aggregated_edges.reshape(nodes.shape[0], nodes.shape[1], -1)
 
             # Update the nodes.
-            nodes_out = node_updater(torch.cat([aggregated_edges, nodes], dim=1))
+            nodes_out = node_updater(torch.cat([aggregated_edges, nodes], dim=-1))
             nodes_out = node_layer_norm(nodes_out)
             nodes = nodes + nodes_out
 
@@ -155,14 +185,22 @@ class GNS(torch.nn.Module):
     """Graph network-based simulator for predicting particle acceleration,
     composed of an Encoder, Processor, and Decoder."""
     def __init__(self, n_materials, physical_dim, n_previous_velocities,
-                 connectivity_radius, box_boundaries=None):
+                 connectivity_radius, box_boundaries=None,
+                 n_processor_layers=10):
         super().__init__()
         self.encoder = Encoder(n_materials, physical_dim, n_previous_velocities,
                                connectivity_radius, box_boundaries=box_boundaries)
-        self.processor = Processor()
+        self.processor = Processor(n_layers=n_processor_layers)
         self.decoder = Decoder(physical_dim)
 
     def forward(self, dp):
         nodes, edges, neighbor_idxs = self.encoder(dp)
         nodes, edges, neighbor_idxs = self.processor(nodes, edges, neighbor_idxs)
-        return self.decoder(nodes)
+        nodes = self.decoder(nodes)
+
+        # The modules return data with the batch dimension, so we remove it
+        # if that's not how it was given to us.
+        if isinstance(dp, TorchDatapoint):
+            return nodes[0]
+        else:
+            return nodes
